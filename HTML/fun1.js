130481844fca03e9c7b9bd81f1d7e35f9e8551e1
mohipal92 <mohipalmaahi4050@gmail.com>
2025-11-08 10:34:43 +0530

    revised all concept of html,css and js

---
+++ HTML/fun1.js
@@ -0,0 +1,56 @@
+// console.log(2+8);
+// const product={
+//         price:890,
+//         model:"car ",
+//         date:"8-29-19"
+
+// }
+// console.log(product.date);
+// let num=prompt("enter a number");
+// if(num%5==0){
+//         console.log(`${num} is divisible by 5`);
+        
+// }
+// else{
+//         ;console.log(`${num} is not divisible by 5`);
+        
+// }
+
+/*let num=9;
+while(true){
+         let a=prompt("guess a number ");
+         if(a==num){
+                console.log("congrats you won");
+                break;
+         }
+         else{
+                
+                continue;
+         }
+}
+*/
+// let fullname=prompt("enter your full name");
+// let user=`@${fullname}${fullname.length}`;
+// console.log(user);
+ 
+/*function help( s){
+           let len=s.length;
+           console.log(len);
+           
+           return len;
+
+
+ }
+ help("mohipalfdd");
+const help1=(s)=>{
+         console.log(s.length);
+         return s.length;
+         
+}
+help("ssdsslksdlsl"); */
+// let arr=[1,2,3,4];
+// arr.forEach((val)=>{
+//                 console.log(val*val);
+                
+// })
+document.getElementById("dom").innerHTML="hello main hu";
